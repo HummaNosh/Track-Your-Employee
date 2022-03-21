1ac9b3756ade950e5586814331b2f1a2e2d4eaf3
Humma Noshine <Hummanoshine@gmail.com>
2022-03-21 00:37:45 +0000

    all working adding in delete employee now

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const Table = require('console.table');
+const Connection = require("mysql/lib/Connection");
 require("dotenv").config();
 
 // Bring in the database connection
@@ -58,43 +59,46 @@ const getStarted = () => {
    if (answers.options === "Add an Employee") {
     addEmp();
     }
+    if (answers.options === "Update an Employee's Role") {
+      updateEmployeeRole();
+      }
 });
 };
 
 
-
+// Get department info as defined in schema/seeds
 function getDept() {
   console.log("its working");
 
     db.query('SELECT * FROM Department', function (err, data) {
       if (err) {
         console.log(`[ERROR]: Failed to get departments`);
-        return res.status(500).json({ success: false });
+        return result.status(500).json({ success: false });
       }
 console.table(data)
 getStarted();
     });
 };
-
+// Get Role info as defined in schema/seeds
 function getRoles() {
 
     db.query('SELECT * FROM Role LEFT JOIN Department ON Role.department_id = Department.id', function (err, data){
       if (err) {
         console.log(`[ERROR]: Failed to get role info | ${err.message}`);
-        return res.status(500).json({ success: false });
+        return result.status(500).json({ success: false });
       }
   
      console.table(data)
      getStarted();
     });
   };
-
+// Get employee data as defined in schema/seeds
 function getEmployee() {
 
     db.query('SELECT * FROM Employee LEFT JOIN Role ON Employee.role_id = Role.id', function (err, data) {
       if (err) {
         console.log(`[ERROR]: Failed to get employee info | ${err.message}`);
-        return res.status(500).json({ success: false });
+        return result.status(500).json({ success: false });
       }
   
      console.table(data)
@@ -102,7 +106,7 @@ function getEmployee() {
     });
   }
 
-
+// this doesnt work
 function addDep() {
 
   inquirer.prompt([
@@ -118,7 +122,6 @@ function addDep() {
   db.query(sql, answers.addDep, (err, result) => {
 
     if (err) throw err;
-    console.log("Added " + answers.addDep + " to departments!");
     getDept();
 
   });
@@ -126,6 +129,7 @@ function addDep() {
  );
 }
 
+// this doesnt work
 function addRoles() {
 
   db.query(`SELECT * FROM Department;`, (err, result) => {
@@ -171,82 +175,136 @@ function addRoles() {
   });
 }
 
+// Add an employee by entering name, role and manager id
+function addEmp () {
+  db.query("SELECT * FROM Role", function (error, result) {
+    if (error) {
+      console.log(error);
+    }
 
-function addEmp() {
-  db.query(`SELECT * FROM Role;`, (err, result) => {
-    if (err) throw err;
-    const role = result.map((Role) => ({ name: Role.title, value: Role.id }));
-    db.query(`SELECT * FROM Employee;`, (err, result) => {
-      if (err) throw err;
-      const Employee = result.map((Employee) => ({
-        name: Employee.first_name + " " + Employee.last_name,
-        value: Employee.id,
-      }));
+    const addNewRoles = result.map(Role => {
+      return {
+        name: Role.title,
+        value: Role.id
+      }
+    })
+    inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "first_name",
+        message: "Please enter the first name of the new employee?",
+      },
+      {
+        type: "input",
+        name: "last_name",
+        message: "Please enter the last name of the new employee",
+      },
+      {
+        type: "list",
+        name: "role_id",
+        message: "Please select the role the new employee is taking on?",
+        choices: addNewRoles,
+      },
+      {
+        type: "input",
+        name: "manager",
+        message: "Please enter the manager's ID",
+    
+      },
+    ])
+    .then((answers) => {
+ 
+      db.query(
+        "INSERT INTO Employee SET ?",
+        {
+          first_name: answers.first_name,
+          last_name: answers.last_name,
+          role_id: answers.role_id,
+          manager_id: answers.manager,
+        },
+        function (error, result) {
+          if (error) {
+            console.log(error);
+          }
+          
+          console.table(result);
+          getStarted();
+        }
+        );
+      });
+    });
+  }
+
+
+
+
+// Update employee roles by using their names..
+
+function updateEmployeeRole() {
+
+  db.query("SELECT * FROM Role", function (error, result) {
+    if (error) {
+      console.log(error);
+    }
+
+
+    const RoleData = result.map(role => {
+      return {
+        name: role.title,
+        value: role.id
+      }
+    })
+
+    db.query("SELECT * FROM Employee", (err, result) => {
+      if(err) {
+        console.log(err);
+        throw err;
+      }
+
+      const EmpData = result.map(Employee => {
+        return {
+          name: Employee.first_name + " " + Employee.last_name,
+          value: Employee.id
+        }
+      });
+
       inquirer
         .prompt([
           {
-            type: "input",
-            name: "firstName",
-            message: "What is the first name of the new employee?",
+            
+            type: "list",
+            name: "Employee",
+            message: "Please select the name of the employee whose role you would like to update",
+            choices: EmpData
           },
           {
-            type: "input",
-            name: "lastName",
-            message: "What is the employee's surname?",
-          },
-          {
+            
             type: "list",
             name: "role",
-            message: "Please select the role the new employee is taking on?",
-            choices: role,
-          },
-          {
-            type: "list",
-            name: "manager",
-            message: "Please select the new employee's manager",
-            choices: Employee,
+            message: "Please select their new role",
+            choices: RoleData
           },
         ])
-        .then((answers) => {
+        .then((result) => {
+          console.log(result.Employee);
+          console.log(result.role);
           db.query(
-            `INSERT INTO Employee SET ?`,
-            {
-              first_name: answers.firstName,
-              last_name: answers.lastName,
-              role_id: answers.role,
-              manager_id: answers.manager,
-            },
-            (err, result) => {
+            "UPDATE Employee SET Employee.role_id = ? WHERE Employee.id = ?",
+            [result.role, result.Employee], (err, result) => {
               if (err) throw err;
-              console.log("Wahoo! Your new employee has been added, please check out 'view all employees' for more information" )
+            
+              console.table(result);
               getStarted();
             }
           );
+        })
+        .catch(err => {
+          console.log(err)
         });
-    });
-  });
-}
 
-  // const updateEmpbyID = (req, res) => {
-  //   const { EmpName } = req.body;
-  //   const { EmpID } = req.params;
-  
-  //   if (!EmpName) {
-  //     return res
-  //       .status(400)
-  //       .json({ success: false, error: 'Please provide a name' });
-  //   }
-  
-  //   req.db.query(
-  //     `UPDATE Roles SET first_name and last_name ="${EmpName}" WHERE id=${EmpID}`,
-  //     (err) => {
-  //       if (err) {
-  //         console.log(`[ERROR]: Failed to update movie | ${err.message}`);
-  //         return res.status(500).json({ success: false });
-  //       }
-  
-  //       return res.json({ success: true });
-  //     }
-  //   );
-  // };
-  
+    });  
+
+  });  
+
+}
